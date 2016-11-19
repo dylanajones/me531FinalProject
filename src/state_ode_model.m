2485fa004a67455f2409409cac4a3a6d7a06c2c5
dylanajones <jonesadylan@gmail.com>
2016-11-19 10:55:17 -0800

    Updating

--- src/state_ode_model.m
+++ src/state_ode_model.m
@@ -2,33 +2,34 @@
 % Note state = [theta, theta_dot, y, y_dot, x, x_dot]
 function state_acc = state_ode_model(t,state)
 
-    m = 5;
-    a = 3;
-    b = 2;
-    r = 0.5;
+    m = 52; %kg
+    a = .75; %m
+    b = .11; %m
+    r = 0.11; %m
     
-    g = 9.8;
-    den = 1000;
+    g = 9.8; %m/s^2
+    den = 1020; %kg/m^3
     
-    B1 = 0;
-    B2 = 0;
+    B1 = 8;
+    B2 = 10;
+    B0 = (m+.2)*g;
     
-    Cd = 0.277;
-    Cl = .5431;
+    Cd = 0.2770;
+    Cl = 0.5431;
     
-    A = 10;
+    A = pi * r^2; %m^2
     
-    l1 = 5;
-    l2 = 5;
+    l1 = .75; %m
+    l2 = .75; %m
 
     I = m / 5 * (a^2 + b^2);
 
     state_acc = [state(2); ... 
                  (B1 * l1 * cos(state(1)) - B2 * l2 * cos(state(2))) / I; ...
                  state(4); ...
-                 (1/m) * (m*g + .5*den*Cd*r*(2*r+(a+b)*abs(cos(state(1))))*state(4) - .5*den*Cl*A*(state(4)^2+state(6)^2)*cos(state(1)) - B1 - B2); ...
+                 (1/m) * (m*g - .5*den*Cd*r*(r*sin(state(1))+(a)*cos(state(1)))*(state(4)) - .5*den*Cl*A*(state(4)^2+state(6)^2)*cos(state(1)) + B1 + B2 - B0); ...
                  state(6); ...
-                 (1/m) *  (.5*den*Cl*A*(state(4)^2+state(6)^2)*sin(state(1)) - .5*den*Cd*r*(2*r+(a+b)*abs(sin(state(1))))*state(6))];
+                 (1/m) *  (.5*den*Cl*A*(state(4)^2+state(6)^2)*sin(state(1)) - .5*den*Cd*r*(r*cos(state(1))+(a)*abs(sin(state(1))))*(state(6)))];
                  
 
 end
